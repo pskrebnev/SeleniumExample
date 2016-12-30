2469ac4dce34c2b94c30fa50082bdecd35cfea92
Pavel Skrebnev <skrebnev@gmail.com>
2016-12-30 13:19:45 +0300

    First test of table page.

--- src/test/java/com/vrp/wd/test/TestTable.java
+++ src/test/java/com/vrp/wd/test/TestTable.java
@@ -1,12 +1,28 @@
 package com.vrp.wd.test;
 
 import com.epam.jdi.uitests.web.selenium.elements.complex.table.Table;
+import com.epam.web.matcher.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.lang.reflect.Method;
+
+import static com.vrp.wd.TablePage.htmlTableExample;
 
 /**
  * Created by Pavel.Skrebnev on 30.12.16.
  */
 public class TestTable extends Table {
 
+    // check the page is opened
+    @BeforeMethod
+    public void before(Method method) {
+        htmlTableExample.isOpened();
+    }
 
-
+    @Test
+    public void getTableInfo() {
+        htmlTableExample.isOpened();
+        Assert.isFalse(htmlTableExample.title.equals("HTML Tables"));
+    }
 }
